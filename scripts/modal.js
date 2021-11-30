cc77639d03454129d2627cbea63c4103190cf5d5
corlay <erwan.corlay@yahoo.fr>
2021-11-30 13:32:12 +0100

    modification btn responsive

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,13 +1,15 @@
 // DOM ELEMENTS MODAL 
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelector("#modal-btn");
+const modalBtn2 = document.querySelector("#modal-btn2");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.getElementsByClassName("close");
-const burger= document.getElementById("burger");
+const burger = document.getElementById("burger");
 
 // ------ DISPLAY MODAL ------ //
 // addEventListener
 modalBtn.addEventListener('click', launchModal);
+modalBtn2.addEventListener('click', launchModal);
 closeBtn[0].addEventListener('click', closeModal);
 burger.addEventListener('click', editNav);
 
